df7434d00081f391aa2d03fbe3b82ad4e93bcaea
Mayuresh12 <mayureshrao@Mayureshs-MacBook-Pro.local>
2015-10-25 01:45:04 -0700

    TextView
    Used Textview ,button and a Lable .When the done button touched the the
    text in the textView is daily in the Label.
    
    ViewController.h used UITextViewDelegate
    
    ViewController.m used
    UITextView,UIButton,UILable
    
    ——>
    _btnDone.hidden=YES (will hide the button until and unless you start
    typing something ).
    ——>[self.tvTextView resignFirstResponder] hides the keyboard when we
    are done with the typing.

--- TextView/ViewController.m
+++ TextView/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *tvTextView;
+@property (weak, nonatomic) IBOutlet UIButton *btnDone;
+@property (weak, nonatomic) IBOutlet UILabel *lblLabel;
+- (IBAction)btnDoneTouched:(id)sender;
 
 @end
 
@@ -17,11 +21,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.tvTextView.delegate=self;
+    self.btnDone.hidden=YES;
+    
 }
+-(void)textViewDidBeginEditing:(UITextView *)textView{
+    _btnDone.hidden=NO;
 
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)btnDoneTouched:(id)sender {
+    self.lblLabel.text= self.tvTextView.text;
+    [self.tvTextView resignFirstResponder];
+}
 @end
